deb0bb7d17648815ce3c7ae6086ac64e16110da7
seunpark <103350263+seunpark06@users.noreply.github.com>
2024-04-17 13:20:46 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.37 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,10 @@
+# 1칸 혹은 2칸을 뛸수 있음
+
+def solution(n):
+    dp = [0] * (n+1)
+    dp[0], dp[1] = 1, 1
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    
+    return dp[n] % 1234567
+        
