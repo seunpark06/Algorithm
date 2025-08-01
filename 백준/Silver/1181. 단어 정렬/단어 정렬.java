c3a73c357339b99ee2cf0cb79c8b53dafda64fa1
seunpark <103350263+seunpark06@users.noreply.github.com>
2025-08-01 17:33:04 +0900

    [Silver V] Title: 단어 정렬, Time: 720 ms, Memory: 56892 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -3,30 +3,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        sc.nextLine(); //줄넘김 제거
+        Scanner scan = new Scanner(System.in);
+        int total_num = scan.nextInt();
+        scan.nextLine(); // 줄넘김 제거
 
-
-        Set<String> set = new HashSet<String>();
-
-        for(int i = 0; i <num ; i++){
-            set.add(sc.nextLine());
+        Set<String> set = new HashSet<>();
+        for(int i = 0; i < total_num ; i++){
+            set.add(scan.nextLine());
         }
 
-        // 정렬을 위해 Array로 변환
-        List<String> sort_str = new ArrayList<>(set);
-        sort_str.sort((a,b) -> {
-            if (a.length() != b.length()) return a.length() - b.length();
+        //정렬을 위해 리스트로 변환
+        List<String> w_list = new ArrayList<>(set);
+
+        w_list.sort((a,b) -> {
+            if(a.length() != b.length()) return a.length() - b.length();
             return a.compareTo(b);
         });
 
-        // a.length() - b.length() 가 양수면 a가 뒤로 감
-        // a.length() - b.length() 가 음수면 a가 앞으로 감
-        
-        //a.compareTo(b) 사전 순서대로 비교
-
-        for (String s: sort_str){
+        for(String s : w_list){
             System.out.println(s);
         }
     }
