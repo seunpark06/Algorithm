62b694aaa82efef17c0df5d435982adb8a846e29
seunpark <103350263+seunpark06@users.noreply.github.com>
2025-08-07 17:18:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.65 ms, Memory: 98 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String p : participant){
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        
+        for(String c : completion){
+            map.put(c, map.get(c) - 1);
+        }
+        
+        for(String r : map.keySet()){
+            if (map.get(r) != 0){
+                return r;
+            }
+        }
+        
+        return "";
+    }
+}
