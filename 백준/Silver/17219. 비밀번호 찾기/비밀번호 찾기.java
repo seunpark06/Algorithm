d36903542c5d5af3a76246c81ca6fe43ea89d396
seunpark <103350263+seunpark06@users.noreply.github.com>
2025-08-30 21:15:21 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 1556 ms, Memory: 145432 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,19 +1,25 @@
+
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt(); //저장된 사이트 수
-        int m = scan.nextInt(); //찾으려는 비밀번호 수
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        int m = 0;
+        n = sc.nextInt();
+        m = sc.nextInt();
 
         Map<String, String> map = new HashMap<>();
+
         for(int i = 0; i < n; i++){
-            map.put(scan.next(), scan.next());
+            map.put(sc.next(), sc.next());
         }
 
         for(int i = 0; i < m; i++){
-            System.out.println(map.get(scan.next()));
+            System.out.println(map.get(sc.next()));
         }
     }
+
 }
