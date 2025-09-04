ae984c5e1609dd7e6a5b0f4b1fdf1bf3cb7abf8b
seunpark <103350263+seunpark06@users.noreply.github.com>
2025-09-04 21:19:05 +0900

    [Silver III] Title: 바이러스, Time: 184 ms, Memory: 17924 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,41 +1,47 @@
+
+
 import java.util.*;
 
 public class Main {
     static ArrayList<Integer>[] graph;
-    static boolean[] visited; //방문 기록용 배열
-    static int count= 0; //바이러스 감염된 컴퓨터 수
+    static boolean[] visited;
+    static int count;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 전체 컴퓨터 수
-        int m = sc.nextInt(); // 연결된 노드 수
-        
-        // 배열 할당
-        graph = new ArrayList[n+1];
-        visited = new boolean[n+1];
 
-        for(int i = 1; i <= n; i++){
-            graph[i] = new ArrayList<>();
-        }
-        // 연결 노드 기록
-        for (int i = 0 ; i < m; i++){
-            int a = sc.nextInt(), b = sc.nextInt();
-            graph[a].add(b);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();//컴퓨터 수
+        int m= sc.nextInt();//연결된 컴퓨터 쌍의 수
+
+
+        // 배열 할당
+        graph = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        //리스트 할당 (1번 노드부터 N번 노드)
+        for(int i=1; i<n+1; i++) graph[i] = new ArrayList<>();
+
+        for(int i = 0; i < m; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a].add(b);// 연결된 노드
             graph[b].add(a);
         }
 
         dfs(1);
-        System.out.println(count -1);
+        System.out.println(count-1);
+
+
+
     }
-
     static void dfs(int node){
+        visited[node] = true;
         count++;
-        visited[node] = true; //방문 표시
 
-        for (int next: graph[node]){ //아직 방문하지 않고 연결된 노드라면
-            if(!visited[next]){
-                dfs(next);
-            }
+        for(int next: graph[node]){
+            if(!visited[next]) dfs(next); //방문하지 않은 노드 방문
         }
     }
+
 }
